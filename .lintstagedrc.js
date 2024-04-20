c071d57aebf446988ad20cf5ef1a7472aba6075a
xarlein <alexvolkovdd@gmail.com>
2024-04-20 14:41:37 +0400

    feat(readme.md): добавлена ссылка на инсталяцию

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-    'src/**/*.[jt]s?(x)': (filenames) =>
-      filenames.length > 10
-        ? 'eslint src --fix --color'
-        : `eslint ${filenames.join(' ')} --fix --color`,
-    'src/**/*.ts?(x)': () => 'tsc --noEmit --pretty -p tsconfig.json',
-  };
-  
+  "src/**/*.[jt]s?(x)": (filenames) =>
+    filenames.length > 10
+      ? "eslint src --fix --color"
+      : `eslint ${filenames.join(" ")} --fix --color`,
+  "src/**/*.ts?(x)": () => "tsc --noEmit --pretty -p tsconfig.json",
+};
